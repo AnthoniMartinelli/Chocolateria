bc9a768d396e2639fff7380fe26b00d1ab7cc3aa
anthoni Bortolotto Martinelli <antho@outlook.com.br>
2021-06-21 13:08:23 -0300

    design base do NovoProduto

--- src/molecules/NovoProduto.jsx
+++ src/molecules/NovoProduto.jsx
@@ -4,18 +4,43 @@ import Typography from "@material-ui/core/Typography";
 import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
+import { makeStyles } from "@material-ui/core";
 import Alfajor from "../atoms/img/Alfajor.png";
 
+const styles = makeStyles(() => ({
+  CardStyle: {
+    display: "flex",
+    flex: "1 0 auto",
+    flexDirection: "column",
+  },
+  CardDivStyle: {
+    textAlign: "center",
+  },
+  CardMediaStyle: {
+    float: "left",
+    height: "100%",
+    paddingBottom: "30%",
+    width: "50%",
+  },
+}));
+
 export default function NovoProduto() {
+  const classes = styles();
   return (
     <ScrollAnimation animateIn="fadeIn" duration={3} delay={500}>
-      <Card>
-        <CardMedia title="imagem alfajor" image={Alfajor} />
+      <Card className={classes.CardStyle}>
         <CardContent>
-          <Typography variant="h5" component="h5">
-            Compre o novo alfajor em uma loja próxima
-          </Typography>
-          <Typography variant="subtitle1">Apenas R$1,99</Typography>
+          <CardMedia
+            title="imagem alfajor"
+            image={Alfajor}
+            className={classes.CardMediaStyle}
+          />
+          <div className={classes.CardDivStyle}>
+            <Typography variant="h5" component="h5">
+              Compre o novo alfajor em uma loja próxima
+            </Typography>
+            <Typography variant="h2">Apenas R$1,99</Typography>
+          </div>
         </CardContent>
       </Card>
     </ScrollAnimation>
