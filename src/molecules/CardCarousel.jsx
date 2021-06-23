08996aa969ef0e2ee3c8abf5af30871eeff8f3b6
anthoni Bortolotto Martinelli <antho@outlook.com.br>
2021-06-23 13:49:48 -0300

    Base do componente ofertas feita

--- src/molecules/CardCarousel.jsx
+++ src/molecules/CardCarousel.jsx
@@ -4,17 +4,34 @@ import Card from "@material-ui/core/Card";
 import CardMedia from "@material-ui/core/CardMedia";
 import CardContent from "@material-ui/core/CardContent";
 import PropTypes from "prop-types";
+import { makeStyles } from "@material-ui/core";
+
+const styles = makeStyles(() => ({
+  CardMediaStyle: {
+    padding: "20%",
+  },
+  CardContentStyle: {
+    textAlign: "center",
+  },
+}));
 
 export default function CardCarousel(props) {
   const { CardMediaTitle, CardMediaLink, ContentTitle, ContentText } = props;
+  const classes = styles();
   return (
     <Card>
-      <CardMedia title={CardMediaTitle} image={CardMediaLink} />
+      <CardMedia
+        title={CardMediaTitle}
+        image={CardMediaLink}
+        className={classes.CardMediaStyle}
+      />
       <CardContent>
         <Typography variant="h6" component="h6">
           {ContentTitle}
         </Typography>
-        <Typography component="p">{ContentText}</Typography>
+        <Typography component="p" className={classes.CardContentStyle}>
+          {ContentText}
+        </Typography>
       </CardContent>
     </Card>
   );
--- src/organisms/Ofertas.jsx
+++ src/organisms/Ofertas.jsx
@@ -5,8 +5,8 @@ import CardCarousel from "../molecules/CardCarousel";
 const breakPoints = [
   { width: 1, itemsToShow: 1 },
   { width: 558, itemsToShow: 2 },
-  { width: 768, itemsToShow: 3 },
-  { width: 1280, itemsToShow: 4 },
+  { width: 768, itemsToShow: 4 },
+  { width: 1280, itemsToShow: 6 },
 ];
 
 export default function Ofertas() {
@@ -19,52 +19,52 @@ export default function Ofertas() {
         ContentText="R$2,99"
       />
       <CardCarousel
-        CardMediaTitle="imagem Trufa Tradicional"
-        CardMediaLink="https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw3b56fa2a/medium/1000111_1.png"
-        ContentTitle="Trufa Tradicional 30g"
+        CardMediaTitle="imagem Trufa Morango"
+        CardMediaLink="https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwdce72a58/medium/1000106_1.png"
+        ContentTitle="Trufa Morango 30g"
         ContentText="R$2,99"
       />
       <CardCarousel
-        CardMediaTitle="imagem Trufa Tradicional"
-        CardMediaLink="https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw3b56fa2a/medium/1000111_1.png"
-        ContentTitle="Trufa Tradicional 30g"
-        ContentText="R$2,99"
+        CardMediaTitle="imagem Trufa zero açucar"
+        CardMediaLink="https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw49f9898d/medium/1000110_1.png"
+        ContentTitle="Trufa zero açucar 30g"
+        ContentText="R$3,50"
       />
       <CardCarousel
-        CardMediaTitle="imagem Trufa Tradicional"
-        CardMediaLink="https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw3b56fa2a/medium/1000111_1.png"
-        ContentTitle="Trufa Tradicional 30g"
-        ContentText="R$2,99"
+        CardMediaTitle="imagem Tablete 55% cacau"
+        CardMediaLink="https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw6e17ca08/medium/1000175_1.png"
+        ContentTitle="Tablete 55% cacau 20g"
+        ContentText="R$3,00"
       />
       <CardCarousel
-        CardMediaTitle="imagem Trufa Tradicional"
-        CardMediaLink="https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw3b56fa2a/medium/1000111_1.png"
-        ContentTitle="Trufa Tradicional 30g"
-        ContentText="R$2,99"
+        CardMediaTitle="imagem Tablete Castanha de caju"
+        CardMediaLink="https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw74f90275/medium/1000151_1.png"
+        ContentTitle="Tablete castanha caju 20g"
+        ContentText="R$2,70"
       />
       <CardCarousel
-        CardMediaTitle="imagem Trufa Tradicional"
-        CardMediaLink="https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw3b56fa2a/medium/1000111_1.png"
-        ContentTitle="Trufa Tradicional 30g"
-        ContentText="R$2,99"
+        CardMediaTitle="imagem Tablete crocante"
+        CardMediaLink="https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dweb855062/medium/1000155_1.png"
+        ContentTitle="Cookie Tablete laranja 20g"
+        ContentText="R$2,70"
       />
       <CardCarousel
-        CardMediaTitle="imagem Trufa Tradicional"
-        CardMediaLink="https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw3b56fa2a/medium/1000111_1.png"
-        ContentTitle="Trufa Tradicional 30g"
-        ContentText="R$2,99"
+        CardMediaTitle="imagem Tablete laranja"
+        CardMediaLink="https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw5e962c72/medium/1000168_1.png"
+        ContentTitle="Tablete Castanha de caju 20g"
+        ContentText="R$2,79"
       />
       <CardCarousel
-        CardMediaTitle="imagem Trufa Tradicional"
-        CardMediaLink="https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw3b56fa2a/medium/1000111_1.png"
-        ContentTitle="Trufa Tradicional 30g"
-        ContentText="R$2,99"
+        CardMediaTitle="imagem Trufa artesanal"
+        CardMediaLink="https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwe96bc8ee/medium/1000104_1.png"
+        ContentTitle="Trufa artesanal 30g"
+        ContentText="R$3,49"
       />
       <CardCarousel
-        CardMediaTitle="imagem Trufa Tradicional"
-        CardMediaLink="https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dw3b56fa2a/medium/1000111_1.png"
-        ContentTitle="Trufa Tradicional 30g"
-        ContentText="R$2,99"
+        CardMediaTitle="imagem Tablete ao leite"
+        CardMediaLink="https://www.cacaushow.com.br/on/demandware.static/-/Sites-masterCatalog_CacauShow/default/dwe836a4ec/medium/1000144_1.png"
+        ContentTitle="Tablete ao leite 20g"
+        ContentText="R$2,00"
       />
     </Carousel>
   );
